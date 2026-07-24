115c541fd4345ec08a1941a9a808969e5fce3f12
psihotrot-tech <psihotrot@gmail.com>
2026-07-24 12:53:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yum-ponude-v11';
+const CACHE='yum-ponude-v12';
 const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));

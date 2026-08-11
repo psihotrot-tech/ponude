bf4980f4ddd56a0609f86b593a1e334e016e686b
psihotrot-tech <psihotrot@gmail.com>
2026-08-11 14:14:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,9 @@
-const CACHE='yum-ponude-v17';
+const CACHE='yum-ponude-v18';
 const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
   if(e.request.mode==='navigate'){
-    // uvek pokušaj internet (nova verzija), keš samo kad nema mreže
-    e.respondWith(
-      fetch(e.request).then(r=>{
-        const cp=r.clone();
-        caches.open(CACHE).then(c=>c.put('./index.html',cp));
-        return r;
-      }).catch(()=>caches.match('./index.html'))
-    );
-    return;
-  }
+    e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp));return r;}).catch(()=>caches.match('./index.html')));return;}
   e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request)));
 });
